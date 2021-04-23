b5264a718edb77053a983696e40bc1bb33a4b407
Fexli Sun @ Plexbio <felix.sun@plexbio.com>
2021-04-23 16:24:00 +0800

    加入新增檔案

---
+++ CamCapture/packageInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace CamCapture
+{
+    public static class packageInfo
+    {
+        /// <summary>
+        /// assembly: AssemblyInformationalVersion
+        /// </summary>
+        public static string Version
+        {
+            get
+            {
+                try
+                {
+                    return FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+
+                }
+                catch
+                {
+                    return "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the application build Version (assembly: AssemblyVersion)
+        /// </summary>
+        /// <value>
+        /// The application build.
+        /// </value>
+        public static string Build
+        {
+            get
+            {
+                try
+                {
+                    return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                }
+                catch
+                {
+                    return "";
+                }
+            }
+        }
+
+
+    }
+}
